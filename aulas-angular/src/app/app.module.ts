3c0ad4014361e0854c292b62e6f2e0820883bd54
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-09-14 22:06:29 -0300

    Aula de angular

---
+++ aulas-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClienteFormComponent } from './clientes/cliente-form/cliente-form.component';
+import { ClienteListComponent } from './clientes/cliente-list/cliente-list.component';
+import { ProdutosListComponent } from './produtos/produtos-list/produtos-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClienteFormComponent,
+    ClienteListComponent,
+    ProdutosListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
