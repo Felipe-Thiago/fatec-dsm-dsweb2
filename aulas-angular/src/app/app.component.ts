9716d0b05ade3e15607e6840df6bd3571b6b0766
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-09-21 22:04:28 -0300

    Criando form com bootstrap no ts

--- aulas-angular/src/app/app.component.ts
+++ aulas-angular/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aulas-angular';
+  title = 'Aulas de Angular';
   mensagem = 'Hello World';
 }
--- aulas-angular/src/app/clientes/cliente-form/cliente-form.component.ts
+++ aulas-angular/src/app/clientes/cliente-form/cliente-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../shared/cliente';
 
 @Component({
   selector: 'app-cliente-form',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cliente-form.component.css']
 })
 export class ClienteFormComponent {
+  'cliente': Cliente;
+  title = '';
 
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    this.title = 'Novo Cliente';
+    this.cliente = new Cliente();
+  }
+
+  onSubmit(){
+    console.log(this.cliente);
+  }
 }
