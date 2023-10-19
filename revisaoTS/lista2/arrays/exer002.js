7f2d39b59f158a6d3c49751c8cd5699f829396bf
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-10-19 16:42:15 -0300

    exercicios revisao

---
+++ revisaoTS/lista2/arrays/exer002.js
@@ -0,0 +1,8 @@
+var vetor1 = [1, 2, 3, 4, 5];
+var vetor2 = [5, 4, 3, 2, 1];
+var vetor3 = [];
+for (var i = 0; i < vetor1.length; i++) {
+    //vetor3.push(vetor1[i] + vetor2[i]);
+    vetor3[i] = vetor1[i] + vetor2[i];
+}
+console.log(vetor3);
