f5643e0745ad2db6a23bcb1808b3e87ff74d24b9
Felipe Thiago <felipethiagodasilva@gmail.com>
2023-11-09 21:14:53 -0300

    Forms angular 2

--- aulas-angular/src/app/clientes/cliente-form/cliente-form.component.ts
+++ aulas-angular/src/app/clientes/cliente-form/cliente-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Cliente } from '../shared/cliente';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-form',
@@ -10,16 +12,19 @@ export class ClienteFormComponent {
   'cliente': Cliente;
   title = '';
 
-  constructor(){
+  constructor(private route: ActivatedRoute, private router: Router) {
     
   }
 
   ngOnInit(){
     this.title = 'Novo Cliente';
     this.cliente = new Cliente();
+
+    const id = this.route.snapshot.paramMap.get('id');
   }
 
   onSubmit(){
     console.log(this.cliente);
+    this.router.navigate(['/clientes']);
   }
 }
